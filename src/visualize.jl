01fcc4f77c5644d3d7a7f74654440accf7968c97
Roger Herikstad <roger.herikstad@gmail.com>
2014-02-24 19:09:22 +0800

    Added function for saving plots

--- src/visualize.jl
+++ src/visualize.jl
@@ -4,6 +4,7 @@ using Spiketrains
 
 type NavigationState
     state::Integer
+    p::FramedPlot
 end
 
 type NavigationControls
@@ -11,7 +12,7 @@ type NavigationControls
 end
 
 function visualize(X)
-    navstate = NavigationState(1)
+    navstate = NavigationState(1, FramedPlot())
     win = Toplevel("Visualize",400,200)
     fwin = Frame(win)
     pack(fwin, expand=true, fill="both")
@@ -29,10 +30,38 @@ function visualize(X)
     set_value(en,string(navstate.state))
     nxt = Button(fctrls,"Next")
     grid(nxt,1,3)
+    sav = Button(fctrls,"Save")
+    grid(sav,1,4)
     bind(prev, "command", path -> plottest(c,navstate,navctrls,X,navstate.state-1))
     bind(nxt, "command", path -> plottest(c,navstate, navctrls, X,navstate.state+1))
     bind(en, "<Return>", path -> updatef(navstate,navctrls,c,X))
-    
+    bind(sav, "command", path -> saveplot(navstate))
+    plottest(c,navstate,navctrls, X,navstate.state) 
+
+end
+
+function saveplot(navstate::NavigationState)
+    cc = navstate.state
+    win = Toplevel()
+    f = Frame(win)
+    pack(f,expand=true,fill="both")
+    l = Label(f, "File name: ")
+    ee = Entry(f, width=15)
+    set_value(ee, "/tmp/cell$cc.pdf")
+    ok = Button(f, "OK")
+    cancel = Button(f, "Cancel")
+    grid(l,1,1)
+    grid(ee, 1,2, pady=5)
+    grid(cancel, 2,1)
+    grid(ok, 2,2)
+
+    function set_close!(navstate::NavigationState)
+        file(navstate.p,string(get_value(ee)))
+        destroy(win)
+    end
+    bind(ee, "<Return>", path->set_close!(navstate))
+    bind(ok,"command", path->set_close!(navstate))
+    bind(cancel, "command", path->destroy(win))
 end
 
 function updatef(navstate::NavigationState, navctrls::NavigationControls,c,X)
@@ -48,6 +77,7 @@ function plottest(c,navstate::NavigationState,navctrls::NavigationControls,X,i::
         reveal(c)
         Tk.update()
         navstate.state = i
+        navstate.p = p
         set_value(navctrls.state, string(i))
     end
 end
